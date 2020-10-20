ee63874607c7ae7a05f695323845ab5b10dcdce5
NicholasSilvaRuiz <bnsilva5@misena.edu.co>
2020-10-20 15:55:31 -0500

    chore: practica

---
+++ java/src/main/java/co/edu/sena/algoritmia/ejercicios/EjercicioP.java
@@ -0,0 +1,22 @@
+package co.edu.sena.algoritmia.ejercicios;
+
+import java.util.Scanner;
+
+public class EjercicioP {
+
+	public static void main(String[] args) {
+		
+		//----- Entrada
+		Scanner teclado = new Scanner(System.in);
+		
+		String []nombres = {"Nicholas", "Silva", "Ruiz"};
+		
+		
+		for(int i=0; i<nombres.length; i++) {
+			System.out.println(nombres[i]);
+		}
+		
+		
+	}
+
+}
